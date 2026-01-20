42368b04f8389c219f65442ffb08ff2d03dff7c7
Smit Patil <smit@smit.codes>
2026-01-20 13:11:02 -0500

    Make blacklist menu keybind configurable

--- src/client/java/codes/smit/ItemphobiaClient.java
+++ src/client/java/codes/smit/ItemphobiaClient.java
@@ -3,6 +3,7 @@ package codes.smit;
 import codes.smit.gui.BlacklistScreen;
 import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import org.lwjgl.glfw.GLFW;
@@ -13,13 +14,12 @@ public class ItemphobiaClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		// Register keybind
-		openGuiKey = new KeyMapping(
+		openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
 				"key.itemphobia.open_gui",
 				InputConstants.Type.KEYSYM,
 				GLFW.GLFW_KEY_I,
 				"category.itemphobia"
-		);
+		));
 
 		Itemphobia.LOGGER.info("Itemphobia client initialized!");
 	}
